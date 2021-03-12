881aae74d5ea659cc5089cb3e7a8b18f224cb41b
avdeevam <avdeev.am471@gmail.com>
2021-03-12 17:16:45 +0300

    feat: add min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (arr == undefined){
+          return 0;
+      }
+      if (arr.length == 0){
+      return 0;
+      }
+    var result = arr[0];
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i] < result) {
+          result = arr[i];
+      }
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+   if (arr == undefined){
+            return 0;
+        }
+        if (arr.length == 0){
+        return 0;
+        }
+      var result = arr[0];
+      for (var i = 0; i < arr.length; i++){
+          if (arr[i] > result){
+              result = arr[i];
+          }
+      }
+      return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+   if (arr == undefined){
+            return 0;
+        }
+        if (arr.length == 0){
+        return 0;
+        }
+  var result = 0;
+  var count = 0;
+  for (var i = 0; i < arr.length; i++){
+          result = result + arr[i];
+          count++;
+      }
+  return result / count;
 }
